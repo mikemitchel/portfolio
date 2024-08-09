72c57e3437177b17782ab5f94a66dcd4895de354
mitch <mitch@nomadiklabs.com>
2024-08-08 20:01:22 -0500

    starter theme

---
+++ src/theme.ts
@@ -0,0 +1,20 @@
+export const theme: Record<string, string> = {
+	/* font and text */
+	'font-body': `Arial, -apple-system, BlinkMacSystemFont, 'Open Sans', sans-serif`,
+	'font-mono': `'Fira Mono', monospace`,
+	'font-size': `16px`,
+	'color-text': `#323336`,
+	'color-text-light': `#dde9f8`,
+	/* main colors */
+	/* removal of rgb() wrapper allows adding opactiy in the app */
+	'color-primary': `64, 117, 166 ` /* #4075a6 */,
+	'color-secondary': `64, 117, 166` /* #40A6A4 */,
+	'color-neutral': `250, 250, 250`,
+	'color-success': 'rgb(15 186 129)',
+	'color-warning': 'rgb(167 127 6)',
+	'color-error': 'rgb(220 20 60)',
+	/* accent */
+	'color-accent': `#a348e9`,
+	'color-accent-subtle': `#e9ecef`,
+	'color-accent-med': `rgb(132, 165, 197)`
+}
